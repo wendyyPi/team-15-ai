8068f6234ffdfdfde49fc92ad09d2e81d7aaf92d
wendyyPi <147113946+wendyyPi@users.noreply.github.com>
2024-09-21 22:05:01 +1000

    add project

---
+++ src/test/java/com/example/ai_generate_report/AiGenerateReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_generate_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGenerateReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
