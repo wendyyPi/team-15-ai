13116a430d7331547518d269b8eb1487499d6984
Leong Yin Chan <u6858120@anu.edu.au>
2024-09-22 02:41:47 +1000

    Finished methods to extract the patient data, and convert it into a prompt

--- src/main/java/com/example/ai_generate_report/Hospital.java
+++ src/main/java/com/example/ai_generate_report/Hospital.java
@@ -1,5 +1,16 @@
 package com.example.ai_generate_report;
 
 public class Hospital {
+    private final String hospitalName = "ANU Hackathon Hospital";
+    private final String address = "Birch Building, 35 Science Rd, Acton ACT 2601";
+    private final String phoneNumber = "0412345678";
 
+    @Override
+    public String toString() {
+        return "Hospital Information: {" +
+                "Hospital Name ='" + hospitalName + '\'' +
+                ", Address ='" + address + '\'' +
+                ", Phone number ='" + phoneNumber + '\'' +
+                '}';
+    }
 }
